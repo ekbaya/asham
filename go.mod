0a0002c8f1d6efa5bef811cc2eed15f99fc71d42
Elias Baya <eliasbaya1223@gmail.com>
2025-04-15 11:03:03 +0300

    pages

--- go.mod
+++ go.mod
@@ -19,6 +19,8 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -28,16 +30,21 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.12.0 // indirect
+	golang.org/x/image v0.21.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	google.golang.org/protobuf v1.36.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
@@ -50,6 +57,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/pdfcpu/pdfcpu v0.9.1
 	golang.org/x/text v0.23.0 // indirect
 	gorm.io/driver/postgres v1.5.11
 )
--- pkg/domain/services/library.go
+++ pkg/domain/services/library.go
@@ -1,8 +1,14 @@
 package services
 
 import (
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
 	"github.com/ekbaya/asham/pkg/db/repository"
-	"github.com/ekbaya/asham/pkg/domain/models"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
 )
 
 type LibraryService struct {
@@ -13,6 +19,85 @@ func NewLibraryService(repo *repository.LibraryRepository) *LibraryService {
 	return &LibraryService{repo: repo}
 }
 
-func (service *LibraryService) FindStandards(params map[string]any, limit, offset int) ([]models.Project, error) {
-	return service.repo.FindStandards(params, limit, offset)
+func (service *LibraryService) FindStandards(params map[string]any, limit, offset int) ([]map[string]any, error) {
+	var standards []map[string]any
+
+	projects, err := service.repo.FindStandards(params, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(projects) > 0 {
+		for _, project := range projects {
+			pageCount := 20 
+			if project.Standard != nil && project.Standard.FileURL != "" {
+				calculatedPages, err := service.getPDFPageCount(project.Standard.FileURL)
+				if err == nil {
+					pageCount = calculatedPages
+				} else {
+					log.Printf("Error calculating PDF pages for standard ID %v: %v", project.ID, err)
+				}
+			}
+
+			standard := map[string]any{
+				"id":             project.ID,
+				"title":          project.Title,
+				"description":    project.Description,
+				"sector":         project.Sector,
+				"committee":      project.TechnicalCommittee.Code,
+				"language":       "English",
+				"published_date": project.PublishedDate,
+				"pages":          pageCount,
+			}
+			standards = append(standards, standard)
+		}
+		return standards, nil
+	} else {
+		return nil, err
+	}
+}
+
+// getPDFPageCount downloads the PDF from the URL and calculates the number of pages
+func (service *LibraryService) getPDFPageCount(pdfURL string) (int, error) {
+	// Parse the URL
+	parsedURL, err := url.Parse(pdfURL)
+	if err != nil {
+		return 0, err
+	}
+
+	// Handle local file path (for assets directory)
+	if parsedURL.Scheme == "" || parsedURL.Scheme == "file" {
+		// Assuming this is a local file path
+		return api.PageCountFile(pdfURL)
+	}
+
+	// For remote URLs, download the file temporarily
+	resp, err := http.Get(pdfURL)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	// Create a temporary file to store the PDF
+	tmpFile, err := os.CreateTemp("", "standard-*.pdf")
+	if err != nil {
+		return 0, err
+	}
+	defer os.Remove(tmpFile.Name()) // Clean up
+
+	// Copy the PDF data to the temporary file
+	_, err = io.Copy(tmpFile, resp.Body)
+	if err != nil {
+		tmpFile.Close()
+		return 0, err
+	}
+	tmpFile.Close()
+
+	// Get the page count from the downloaded file
+	pageCount, err := api.PageCountFile(tmpFile.Name())
+	if err != nil {
+		return 0, err
+	}
+
+	return pageCount, nil
 }
