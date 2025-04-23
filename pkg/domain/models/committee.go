7fd896a15aea84ce951e4d199a33790eadc479f6
Elias Baya <Eliasbaya122@gmail.com>
2025-04-23 22:01:51 +0300

    deploy

--- pkg/domain/models/committee.go
+++ pkg/domain/models/committee.go
@@ -4,7 +4,6 @@ import (
 	"github.com/google/uuid"
 )
 
-// Committee is a base struct for different committees
 type Committee struct {
 	ID            uuid.UUID `json:"id"`
 	Name          string    `json:"name" binding:"required"`
