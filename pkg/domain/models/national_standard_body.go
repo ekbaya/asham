1530d190156b44e4724a28bd58bac0724cc11475
Elias Baya <Eliasbaya122@gmail.com>
2025-04-23 21:07:17 +0300

    foreign key

--- pkg/domain/models/national_standard_body.go
+++ pkg/domain/models/national_standard_body.go
@@ -10,11 +10,11 @@ import (
 type NationalStandardBody struct {
 	ID                    uuid.UUID    `json:"id"`
 	Name                  string       `json:"name" binding:"required" gorm:"unique"`
-	NationalTCSecretaryID *string      `json:"national_tc_secretary_id" binding:"required"`
-	NationalTCSecretary   *Member      `json:"national_tc_secretary"`
+	NationalTCSecretaryID *string      `json:"national_tc_secretary_id" binding:"required" gorm:"column:national_tc_secretary_id"`
+	NationalTCSecretary   *Member      `json:"national_tc_secretary" gorm:"foreignKey:NationalTCSecretaryID;references:ID"`
 	MemberStateID         string       `json:"member_state_id" binding:"required"`
-	MemberState           *MemberState `json:"member_state"`
-	Members               *[]Member    `json:"members" `
+	MemberState           *MemberState `json:"member_state" gorm:"foreignKey:MemberStateID;references:ID"`
+	Members               *[]Member    `json:"members"`
 	TelephoneNumber       string       `json:"telephone_number" example:"+254700000000"`
 	CreatedAt             time.Time    `json:"created_at"`
 }
